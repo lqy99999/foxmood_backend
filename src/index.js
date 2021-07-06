69908569c569fce6a3b3137c95f241dd0e805859
lqy99999 <lqy907753302@gmail.com>
2021-07-07 02:54:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga';
-const { ApolloServer, gql } = require('apollo-server');
 import db from './db';
 import User from '../resolvers/User'
 import Message from '../resolvers/Message'
@@ -9,16 +8,14 @@ import Vote from '../resolvers/Vote'
 import Query from '../resolvers/Query';
 import Mutation from '../resolvers/Mutation';
 import Subscription from '../resolvers/Subscription';
-const expressPlayground = require("graphql-playground-middleware-express").default;
-const express = require("express");
+const { ApolloServer, gql } = require('apollo-server');
+import {schema} from './schema'
 
-// const schema = require("./schema");
 const pubsub = new PubSub();
 const mongo = require('../mongo');
-const port = process.env.PORT || 5000;
 
-const server = new GraphQLServer({
-  typeDefs: './schema.graphql',
+const server = new ApolloServer({
+  typeDefs: schema,
   resolvers: {
     User,
     Message,
@@ -35,46 +32,15 @@ const server = new GraphQLServer({
   },
 });
 
+
 mongo.connect(); // from mongo.js
 
-// const resolvers = {
-//   User,
-//   Message,
-//   Post,
-//   Comment,
-//   Vote,
-//   Query,
-//   Mutation,
-//   Subscription,
-// }
-// const server = new ApolloServer({ typeDefs:'./schema.graphql', resolvers });
-// const context = {db, pubsub}
-
-// const app = express();
-// server.applyMiddleware({ app });
-// // app.use(cors())
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema,
-//     rootValue: resolvers,
-//     context,
-//   })
-// );
-
-// //Graphql Playground route
-// app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
-
-// app.listen(port, () => {
-//     console.log((`The server is up on port ${port}!`));
-//   });
 
 
-
-server.start({ port: process.env.PORT | 5000 }, () => {
-  console.log(`The server is up on port ${process.env.PORT | 5000}!`);
-});
-
-// server.listen(8080, () => {
-//   console.log('Server listening at http://localhost:8080');
+// server.start({ port: process.env.PORT | 5000 }, () => {
+//   console.log(`The server is up on port ${process.env.PORT | 5000}!`);
 // });
+
+server.listen({ port: process.env.PORT | 5000 }, () => {
+  console.log('Server listening');
+});
--- src/schema.js
+++ src/schema.js
@@ -1,178 +1,178 @@
 const { buildSchema } = require("graphql");
-const { ApolloServer, gql } = require('apollo-server-express');
+// const { ApolloServer, gql } = require('apollo-server-express');
+const { gql } = require('apollo-server')
 
-const schema = buildSchema(`
-type Query {
-    users(name: String): [User!]
-    signIn(name: String!, password: String!): SignInType
-    posts(type: Int, author: String): [Post!]
-    onemessageboxes: [Message!]
-    votes(vote: String): [Vote!]
-  }
-  
-  type Mutation {
-    createUser(data: CreateUserInput!): SignUpType
-    deleteUser(name: String!): User!
-    updateUser(data: UpdateUserInput!): User!
-  
-    createPost(data: CreatePostInput!): Post!
-    deletePost(type: Int!, _id: ID!, author: String!): Post!
-    createComment(data: CreateCommentInput!): Comment!
-    deleteComment(data: DeleteCommentInput!): Comment!
-  
-    createOneMessage(sender: String!, body: String!): Message!
-    updateOneMessage(sender: String!, body: String!): Message!
-  
-    createVote(data: CreateVoteInput!): Vote!
-    updateVote(data: CreateVoteInput!): Vote!
-    deleteVote(user: String!): Vote!
-  
-    clearData(type: String): Boolean!
-  }
-  
-  type Subscription {
-    user: UserSubscriptionPayload!
-    post(type: Int, author: String): PostSubscriptionPayload!
-    post6(type: Int): PostSubscriptionPayload!
-    oneMessage: OneMessageSubscriptionPayload!
-    vote: VoteSubscriptionPayload!
-  }
-  
-  type User {
-    name: String!
-    password: String!
-    friends: [Friend!]
-    mood: [Int!]
-    today: Int!
-  }
-  
-  type Friend{
-    _id: ID!
-    name: String!
-    today: Int!
-  }
-  
-  type Message {
-    # type: Int!
-    date: String!
-    sender: Sender!
-    body: String!
-  }
-  
-  type Sender {
-    name: String!
-  }
-  
-  type Post {
-    _id: ID!
-    type: Int!
-    time: String!
-    body: String!
-    author: Author!
-    comments: [Comment!]
-  }
-  
-  type Comment {
-    _id: ID!
-    post: Author!
-    time: String!
-    body: String!
-    author: Author!
-  }
-  
-  type Author {
-    name: String!
-  }
-  
-  type Vote {
-    vote: String!
-    creator: Name!
-    count: Int!
-  }
-  
-  type Name {
-    name: String!
-  }
-  
-  type UserSubscriptionPayload {
-    mutation: MutationType!
-    data: User!
-  }
-  
-  type PostSubscriptionPayload {
-    mutation: MutationType!
-    data: Post!
-  }
-  
-  type VoteSubscriptionPayload {
-    mutation: MutationType!
-    data: Vote!
-  }
-  
-  type OneMessageSubscriptionPayload{
-    mutation: MutationType!
-    sender: String!
-    body: String!
-  }
-  
-  input CreateUserInput {
-    name: String!
-    password: String!
-  }
-  
-  input UpdateUserInput {
-    name: String!
-    friends: [String!]
-    mood: [Int!]
-    today: Int!
-  }
-  
-  input CreatePostInput {
-    type: Int!
-    body: String!
-    author: String!
-  }
-  
-  input CreateCommentInput {
-    type: Int!
-    postId: ID!
-    postAuthor: String!
-    body: String!
-    author: String!
-  }
-  
-  input DeleteCommentInput {
-    type: Int!
-    postId: Int!
-    postAuthor:String!
-    commentId: Int!
-    author: String!
-  }
-  
-  input CreateVoteInput {
-    vote: String!
-    creator: String!
-  }
-  
-  enum MutationType {
-    CREATED
-    UPDATED
-    DELETED
-    ADDED_COMMENT
-    DELETED_COMMENT
-  }
-  
-  enum SignInType {
-    SUCCESS
-    USER_NOT_FOUND 
-    USER_EXISTS
-    NOT_MATCH
-  }
-  
-  enum SignUpType {
-    SUCCESS
-    USER_EXISTS
-  }  
-`);
+const schema = gql`
+  type Query {
+      users(name: String): [User!]
+      signIn(name: String!, password: String!): SignInType
+      posts(type: Int, author: String): [Post!]
+      onemessageboxes: [Message!]
+      votes(vote: String): [Vote!]
+    }
+    
+    type Mutation {
+      createUser(data: CreateUserInput!): SignUpType
+      deleteUser(name: String!): User!
+      updateUser(data: UpdateUserInput!): User!
+    
+      createPost(data: CreatePostInput!): Post!
+      deletePost(type: Int!, _id: ID!, author: String!): Post!
+      createComment(data: CreateCommentInput!): Comment!
+      deleteComment(data: DeleteCommentInput!): Comment!
+    
+      createOneMessage(sender: String!, body: String!): Message!
+      updateOneMessage(sender: String!, body: String!): Message!
+    
+      createVote(data: CreateVoteInput!): Vote!
+      updateVote(data: CreateVoteInput!): Vote!
+      deleteVote(user: String!): Vote!
+    
+      clearData(type: String): Boolean!
+    }
+    
+    type Subscription {
+      user: UserSubscriptionPayload!
+      post(type: Int, author: String): PostSubscriptionPayload!
+      post6(type: Int): PostSubscriptionPayload!
+      oneMessage: OneMessageSubscriptionPayload!
+      vote: VoteSubscriptionPayload!
+    }
+    
+    type User {
+      name: String!
+      password: String!
+      friends: [Friend!]
+      mood: [Int!]
+      today: Int!
+    }
+    
+    type Friend{
+      _id: ID!
+      name: String!
+      today: Int!
+    }
+    
+    type Message {
+      # type: Int!
+      date: String!
+      sender: Sender!
+      body: String!
+    }
+    
+    type Sender {
+      name: String!
+    }
+    
+    type Post {
+      _id: ID!
+      type: Int!
+      time: String!
+      body: String!
+      author: Author!
+      comments: [Comment!]
+    }
+    
+    type Comment {
+      _id: ID!
+      post: Author!
+      time: String!
+      body: String!
+      author: Author!
+    }
+    
+    type Author {
+      name: String!
+    }
+    
+    type Vote {
+      vote: String!
+      creator: Name!
+      count: Int!
+    }
+    
+    type Name {
+      name: String!
+    }
+    
+    type UserSubscriptionPayload {
+      mutation: MutationType!
+      data: User!
+    }
+    
+    type PostSubscriptionPayload {
+      mutation: MutationType!
+      data: Post!
+    }
+    
+    type VoteSubscriptionPayload {
+      mutation: MutationType!
+      data: Vote!
+    }
+    
+    type OneMessageSubscriptionPayload{
+      mutation: MutationType!
+      sender: String!
+      body: String!
+    }
+    
+    input CreateUserInput {
+      name: String!
+      password: String!
+    }
+    
+    input UpdateUserInput {
+      name: String!
+      friends: [String!]
+      mood: [Int!]
+      today: Int!
+    }
+    
+    input CreatePostInput {
+      type: Int!
+      body: String!
+      author: String!
+    }
+    
+    input CreateCommentInput {
+      type: Int!
+      postId: ID!
+      postAuthor: String!
+      body: String!
+      author: String!
+    }
+    
+    input DeleteCommentInput {
+      type: Int!
+      postId: Int!
+      postAuthor:String!
+      commentId: Int!
+      author: String!
+    }
+    
+    input CreateVoteInput {
+      vote: String!
+      creator: String!
+    }
+    
+    enum MutationType {
+      CREATED
+      UPDATED
+      DELETED
+      ADDED_COMMENT
+      DELETED_COMMENT
+    }
+    
+    enum SignInType {
+      SUCCESS
+      USER_NOT_FOUND 
+      USER_EXISTS
+      NOT_MATCH
+    }
+    
+    enum SignUpType {
+      SUCCESS
+      USER_EXISTS
+    }`
 
-module.exports = schema;
+export {schema}
