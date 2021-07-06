f51a57c18d1259b6378fbc2dc7c3b0e32801c73a
lqy99999 <lqy907753302@gmail.com>
2021-07-07 01:32:28 +0800

    update

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.use(
 app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
 
-const port = process.env.PORT || "5000";
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log((`The server is up on port ${port}!`));
